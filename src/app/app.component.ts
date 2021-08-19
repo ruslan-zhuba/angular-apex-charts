09120796f3b07437a4a6ed18122ac5fa84ab5d89
Ruslan Zhubanishchev <r.zhubanishchev@12storeez.com>
2021-08-19 17:43:49 +0300

    apex preview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-apex-chart';
+  // @ViewChild("chart", { static: false }) chart: ChartComponent;
+  private data = [10, 41, 35, 51, 49, 62, 69, 91, 148];
+  public chartOptions: ChartOptions = {
+    series: [
+      {
+        name: "My-series",
+        data: this.data
+      }
+    ],
+    chart: {
+      height: 350,
+      type: "bar"
+    },
+    title: {
+      text: "My First Angular Chart"
+    },
+    xaxis: {
+      categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
+    }
+  };
+
+  public updateSeries() {
+    const newValue = Math.floor(Math.random() * 100);
+    this.data = [...this.data, newValue];
+    this.chartOptions.series = [{
+      data: this.data
+    }];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgApexchartsModule} from "ng-apexcharts";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgApexchartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
